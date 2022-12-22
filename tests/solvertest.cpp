86ffbe8fa7ce932b689ed46e080b17f7c4e2bc83
Trevor Schlack <schlackt@gmail.com>
2022-12-22 15:53:59 -0500

    Add function to solve for systematic errors

---
+++ tests/solvertest.cpp
@@ -0,0 +1,152 @@
+// From: Testing Unconstrained Optimization Software by Jorge J. More, Burton S.
+// Garbow and Kenneth E. Hillstrom in ACM Transactions on Mathematical Software,
+// Vol 7(1), March 1981.
+#include <cmath>
+#include <ceres/ceres.h>
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+using ceres::AutoDiffCostFunction;
+using ceres::CostFunction;
+using ceres::Problem;
+using ceres::Solve;
+using ceres::Solver;
+struct F1 {
+    double phinorm1;
+    double sep;
+    double r1;
+    double thetanorm1;
+    double phi1p;
+    template <typename T>
+    bool operator()(const T* const theta0, const T* const phi0, const T* const phi0p, T* residual) const {
+        residual[0] = phinorm1 + phi0[0] - (sep * cos(phinorm1 + phi0[0]) / r1 / sin(thetanorm1 + theta0[0])) + phi0p[0] - phi1p;
+        return true;
+    }
+};
+struct F2 {
+    double thetanorm1;
+    double sep;
+    double phinorm1;
+    double r1;
+    double theta1p;
+    template <typename T>
+    bool operator()(const T* const theta0, const T* const phi0, const T* const theta0p, T* residual) const {
+        residual[0] = thetanorm1 + theta0[0] - (sep * cos(thetanorm1 + theta0[0]) * sin(phinorm1 + phi0[0]) / r1) - theta0p[0] - theta1p;
+        return true;
+    }
+};
+struct F3 {
+    double phinorm2;
+    double sep;
+    double r2;
+    double thetanorm2;
+    double phi2p;
+    template <typename T>
+    bool operator()(const T* const theta0, const T* const phi0, const T* const phi0p, T* residual) const {
+        residual[0] = phinorm2 + phi0[0] - (sep * cos(phinorm2 + phi0[0]) / r2 / sin(thetanorm2 + theta0[0])) + phi0p[0] - phi2p;
+        return true;
+    }
+};
+struct F4 {
+    double thetanorm2;
+    double sep;
+    double phinorm2;
+    double r2;
+    double theta2p;
+    template <typename T>
+    bool operator()(const T* const theta0, const T* const phi0, const T* const theta0p, T* residual) const {
+        residual[0] = thetanorm2 + theta0[0] - (sep * cos(thetanorm2 + theta0[0]) * sin(phinorm2 + phi0[0]) / r2) - theta0p[0] - theta2p;
+        return true;
+    }
+};
+DEFINE_string(minimizer,
+"trust_region",
+"Minimizer type to use, choices are: line_search & trust_region");
+void solve(double yhfov, double xhfov, int Nx, int Ny, double sep, double panstart, double panend, double tiltstart, double tiltend, double r1, int x1, int y1, double theta1m, double phi1m, double r2, int x2, int y2, double theta2m, double phi2m, int argc, char** argv) {
+    GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
+    google::InitGoogleLogging(argv[0]);
+    double theta0 = EIGEN_PI / 6;
+    double phi0 = EIGEN_PI / 6;
+    double theta0p = EIGEN_PI / 6;
+    double phi0p = EIGEN_PI / 6;
+    Problem problem;
+    // Add residual terms to the problem using the autodiff
+    // wrapper to get the derivatives automatically. The parameters, x1 through
+    // x4, are modified in place.
+    double phinorm1 = atan(2 * x1 * tan(xhfov) / Nx);
+    double thetanorm1 = atan((float)Ny / 2 / (float)y1 / tan(yhfov));
+    double phinorm2 = atan(2 * x2 * tan(xhfov) / Nx);
+    double thetanorm2 = atan((float)Ny / 2 / (float)y2 / tan(yhfov));
+    double theta1p = 2 * EIGEN_PI * theta1m / 3 / (tiltend - tiltstart);
+    double phi1p = EIGEN_PI * ((phi1m / (panend - panstart)) - 0.5);
+    double theta2p = 2 * EIGEN_PI * theta2m / 3 / (tiltend - tiltstart);
+    double phi2p = EIGEN_PI * ((phi2m / (panend - panstart)) - 0.5);
+
+    F1 * f1 = new F1;
+    f1->phinorm1 = phinorm1;
+    f1->sep = sep;
+    f1->r1 = r1;
+    f1->thetanorm1 = thetanorm1;
+    f1->phi1p = phi1p;
+
+    F2 * f2 = new F2;
+    f2->phinorm1 = phinorm1;
+    f2->sep = sep;
+    f2->r1 = r1;
+    f2->thetanorm1 = thetanorm1;
+    f2->theta1p = theta1p;
+
+    F3 * f3 = new F3;
+    f3->phinorm2 = phinorm2;
+    f3->sep = sep;
+    f3->r2 = r2;
+    f3->thetanorm2 = thetanorm2;
+    f3->phi2p = phi2p;
+
+    F4 * f4 = new F4;
+    f4->phinorm2 = phinorm2;
+    f4->sep = sep;
+    f4->r2 = r2;
+    f4->thetanorm2 = thetanorm2;
+    f4->theta2p = theta2p;
+
+    problem.AddResidualBlock(
+            new AutoDiffCostFunction<F1, 1, 1, 1, 1>(f1), nullptr, &theta0, &phi0, &phi0p);
+    problem.AddResidualBlock(
+            new AutoDiffCostFunction<F2, 1, 1, 1, 1>(f2), nullptr, &theta0, &phi0, &theta0p);
+    problem.AddResidualBlock(
+            new AutoDiffCostFunction<F3, 1, 1, 1, 1>(f3), nullptr, &theta0, &phi0, &phi0p);
+    problem.AddResidualBlock(
+            new AutoDiffCostFunction<F4, 1, 1, 1, 1>(f4), nullptr, &theta0, &phi0, &theta0p);
+    Solver::Options options;
+    LOG_IF(FATAL,
+           !ceres::StringToMinimizerType(CERES_GET_FLAG(FLAGS_minimizer),
+                                         &options.minimizer_type))
+    << "Invalid minimizer: " << CERES_GET_FLAG(FLAGS_minimizer)
+    << ", valid options are: trust_region and line_search.";
+    options.max_num_iterations = 100;
+    options.linear_solver_type = ceres::DENSE_QR;
+    options.minimizer_progress_to_stdout = true;
+    // clang-format off
+    std::cout << "Initial theta0 = " << theta0
+              << ", phi0 = " << phi0
+              << ", theta0p = " << theta0p
+              << ", phi0p = " << phi0p
+              << "\n";
+    // clang-format on
+    // Run the solver!
+    Solver::Summary summary;
+    Solve(options, &problem, &summary);
+    std::cout << summary.FullReport() << "\n";
+    // clang-format off
+    std::cout << "Final theta0 = " << theta0
+              << ", phi0 = " << phi0
+              << ", theta0p = " << theta0p
+              << ", phi0p = " << phi0p
+              << "\n";
+    // clang-format on
+}
+
+int main(int argc, char** argv) {
+    solve(EIGEN_PI/5, EIGEN_PI/4, 346, 260, 1, 0, 300, 0, 450, 1000, -45, 60, 100, 300, 1000, 50, 75, 270, 325, argc, argv);
+    return 0;
+}
