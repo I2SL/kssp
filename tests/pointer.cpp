0b91e26a07e6f9274a85d8b6f4dcb0934e7b27e0
Trevor Schlack <schlackt@gmail.com>
2022-12-25 16:17:54 -0500

    Add systematic error calibration to pointer.cpp

--- tests/pointer.cpp
+++ tests/pointer.cpp
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <iostream>
 #include <cmath>
 #include <thread>
@@ -8,32 +10,7 @@
 int main () {
     std::mutex mtx;
     bool active = true;
-
-    Stage kessler("192.168.50.1", 5520);
-    kessler.handshake();
-    std::cout << kessler.get_device_info().to_string();
-    kessler.reset_axis(0);
-    std::thread driver(controller, std::ref(kessler));
-    std::thread pinger(ping, std::ref(kessler), std::ref(mtx), std::ref(active));
-
-    printf("Move Slide motor to start position and press 'Q'. Then move Slide motor to end position and press `Q`.\n");
-    calibrate(1, kessler, mtx);
-    printf("Move Pan motor to start position and press 'Q'. Then move Pan motor to end position and press `Q`.\n");
-    calibrate(2, kessler, mtx);
-    printf("Move Tilt motor to start position and press 'Q'. Then move Tilt motor to end position and press `Q`.\n");
-    calibrate(3, kessler, mtx);
-    printf("Calibration complete. Press space to exit manual control.\n");
-    driver.join();
-
-    class MotorInfo info = kessler.get_motor_info();
-    std::cout << info.to_string();
-    float begin_pan = info.motors[1].begin_position;
-    float end_pan = info.motors[1].end_position;
-    float begin_tilt = info.motors[2].begin_position;
-    float end_tilt = info.motors[2].end_position;
-    end_pan -= begin_pan;
-    end_tilt -= begin_tilt;
-
+    std::string correction("n");
     double hfovx;
     double hfovy;
     double px;
@@ -60,6 +37,40 @@ int main () {
     hfovx = atan(px * nx / f / 2);
     hfovy = atan(px * ny / f / 2);
 
+    Stage kessler("192.168.50.1", 5520);
+    kessler.handshake();
+    std::cout << kessler.get_device_info().to_string();
+    kessler.reset_axis(0);
+    std::thread driver(controller, std::ref(kessler));
+    std::thread pinger(ping, std::ref(kessler), std::ref(mtx), std::ref(active));
+
+    printf("Move Slide motor to start position and press 'Q'. Then move Slide motor to end position and press `Q`.\n");
+    calibrate(1, kessler, mtx);
+    printf("Move Pan motor to start position and press 'Q'. Then move Pan motor to end position and press `Q`.\n");
+    calibrate(2, kessler, mtx);
+    printf("Move Tilt motor to start position and press 'Q'. Then move Tilt motor to end position and press `Q`.\n");
+    calibrate(3, kessler, mtx);
+
+    class MotorInfo info = kessler.get_motor_info();
+    std::cout << info.to_string();
+    float begin_pan = info.motors[1].begin_position;
+    float end_pan = info.motors[1].end_position;
+    float begin_tilt = info.motors[2].begin_position;
+    float end_tilt = info.motors[2].end_position;
+
+    printf("Calibrate for systematic errors? (y/n)\n");
+    std::cin >> correction;
+    float theta0, phi0, theta0p, phi0p = 0;
+    if (correction == "y") {
+        double r1, theta1m, phi1m, r2, theta2m, phi2m;
+        int x1, y1, x2, y2;
+        std::tie(theta1m, phi1m, r1, x1, y1) = get_calibration_point(kessler, mtx);
+        std::tie(theta2m, phi2m, r2, x2, y2) = get_calibration_point(kessler, mtx);
+        std::tie(theta0, phi0, theta0p, phi0p) = solve(hfovy, hfovx, nx, ny, y0, begin_pan, end_pan, begin_tilt, end_tilt, r1, x1, y1, theta1m, phi1m, r2, x2, y2, theta2m, phi2m);
+    }
+    printf("Calibration complete. Press space to exit manual control.\n");
+    driver.join();
+
     while (true) {
         printf("Enter target distance (or 0 to quit):\n");
         std::cin >> r;
@@ -71,18 +82,18 @@ int main () {
         printf("Enter target y coordinate:\n");
         std::cin >> y;
 
-        double theta = get_theta(y, ny, hfovy);
-        double phi = get_phi(x, nx, hfovx);
-        double theta_prime = get_theta_prime(phi, theta, y0, r);
-        double phi_prime = get_phi_prime(phi, theta, y0, r);
+        double theta = get_theta(y, ny, hfovy, theta0);
+        double phi = get_phi(x, nx, hfovx, phi0);
+        double theta_prime = get_theta_prime(phi, theta, y0, r, theta0p);
+        double phi_prime = get_phi_prime(phi, theta, y0, r, phi0p);
 
         printf("Calculated Coordinates in degrees (theta, phi):\n");
         printf("EBS: (%.2Lf, %.2Lf)\n", theta * 180 / EIGEN_PI, phi * 180 / EIGEN_PI);
         printf("FBC: (%.2Lf, %.2Lf)\n", theta_prime * 180 / EIGEN_PI, phi_prime * 180 / EIGEN_PI);
         printf("\n");
 
-        float pan_position = end_pan * (float)(phi_prime / EIGEN_PI) + (float)0.5 * end_pan;
-        float tilt_position = 3 * end_tilt * (float)(theta_prime / EIGEN_PI) / 2;
+        float pan_position = get_pan_position(begin_pan, end_pan, phi_prime);
+        float tilt_position = get_tilt_position(begin_tilt, end_tilt, theta_prime);
 
         printf("Target motor positions:\n");
         printf("Pan: %.2f (Range: 0 - %.2f)\n", pan_position, end_pan);
--- tests/pointer_utils.hpp
+++ tests/pointer_utils.hpp
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <algorithm>
 #include <cmath>
 #include <ctime>
 #include <thread>
+#include <tuple>
 
 #include <ceres/ceres.h>
 #include <gflags/gflags.h>
@@ -67,9 +69,7 @@ struct F4 {
 DEFINE_string(minimizer,
               "trust_region",
               "Minimizer type to use, choices are: line_search & trust_region");
-void solve(double yhfov, double xhfov, int Nx, int Ny, double sep, double panstart, double panend, double tiltstart, double tiltend, double r1, int x1, int y1, double theta1m, double phi1m, double r2, int x2, int y2, double theta2m, double phi2m, int argc, char** argv) {
-    GFLAGS_NAMESPACE::ParseCommandLineFlags(&argc, &argv, true);
-    google::InitGoogleLogging(argv[0]);
+std::tuple<float, float, float, float> solve(double yhfov, double xhfov, int Nx, int Ny, double sep, double panstart, double panend, double tiltstart, double tiltend, double r1, int x1, int y1, double theta1m, double phi1m, double r2, int x2, int y2, double theta2m, double phi2m) {
     double theta0 = 0;
     double phi0 = 0;
     double theta0p = 0;
@@ -150,6 +150,8 @@ void solve(double yhfov, double xhfov, int Nx, int Ny, double sep, double pansta
               << ", phi0p = " << phi0p
               << "\n";
     // clang-format on
+    std::tuple<float, float, float, float> errors((float)theta0, (float)phi0, (float)theta0p, (float)phi0p);
+    return errors;
 }
 
 // Drive stage with manual controls
@@ -234,11 +236,10 @@ void controller (Stage& kessler)
 void calibrate(boost::uint8_t motor_address, Stage& kessler, std::mutex& mtx) {
     bool start = false;
     bool end = false;
-    bool q_pressed = false;
     while(!kessler.MotorCalibratedQueue.empty()) kessler.MotorCalibratedQueue.pop();
     printf("Press `Q` to mark Motor %hd start position.\n", motor_address);
     while (!start) {
-        q_pressed = GetAsyncKeyState(0x51);
+        bool q_pressed = GetAsyncKeyState(0x51);
         if (q_pressed) {
             mtx.lock();
             kessler.mark_begin_position(motor_address);
@@ -251,7 +252,7 @@ void calibrate(boost::uint8_t motor_address, Stage& kessler, std::mutex& mtx) {
 
     printf("Press `Q` to mark Motor %hd end position.\n", motor_address);
     while (!end) {
-        q_pressed = GetAsyncKeyState(0x51);
+        bool q_pressed = GetAsyncKeyState(0x51);
         if (q_pressed) {
             mtx.lock();
             kessler.mark_end_position(motor_address);
@@ -262,7 +263,7 @@ void calibrate(boost::uint8_t motor_address, Stage& kessler, std::mutex& mtx) {
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
     while(kessler.MotorCalibratedQueue.empty()){
-        continue;
+        //do nothing until message received
     }
 
     class MotorCalibrated info = kessler.MotorCalibratedQueue.front();
@@ -288,18 +289,69 @@ void ping(Stage& kessler, std::mutex& mtx, bool& active) {
     }
 }
 
-double get_phi(int x, int nx, double hfov) {
-    return atan(2 * x * tan(hfov) / nx);
+std::tuple<double, double, double, int, int> get_calibration_point(Stage& kessler, std::mutex& mtx) {
+    float pan_position;
+    float tilt_position;
+    double r;
+    int x;
+    int y;
+    printf("Center camera on known target and press `Q`.\n");
+    while (true) {
+        bool q_pressed = GetAsyncKeyState(0x51);
+        if (q_pressed) {
+            mtx.lock();
+            class MotorInfo info = kessler.get_motor_info();
+            pan_position = info.motors[1].position;
+            tilt_position = info.motors[2].position;
+            mtx.unlock();
+            break;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    printf("Enter target distance (m):\n");
+    std::cin >> r;
+    printf("Enter target x coordinate:\n");
+    std::cin >> x;
+    printf("Enter target y coordinate:\n");
+    std::cin >> y;
+    std::tuple<double, double, double, int, int> ret{(double)tilt_position, (double)pan_position, r, x, y};
+    return ret;
 }
 
-double get_theta(int y, int ny, double hfov) {
-    return atan((float)ny / 2 / (float)y / tan(hfov));
+double get_phi(int x, int nx, double hfov, double phi0) {
+    return atan(2 * x * tan(hfov) / nx) + phi0;
 }
 
-double get_phi_prime(double phi, double theta, double y0, double r) {
-    return phi - (y0 / r) * (cos(phi) / sin(theta));
+double get_theta(int y, int ny, double hfov, double theta0) {
+    return atan((float)ny / 2 / (float)y / tan(hfov)) + theta0;
 }
 
-double get_theta_prime(double phi, double theta, double y0, double r) {
-    return theta - (y0 / r) * cos(theta) * sin(phi);
+double get_phi_prime(double phi, double theta, double y0, double r, double phi0p) {
+    return phi - (y0 / r) * (cos(phi) / sin(theta)) + phi0p;
+}
+
+double get_theta_prime(double phi, double theta, double y0, double r, double theta0p) {
+    return theta - (y0 / r) * cos(theta) * sin(phi) + theta0p;
+}
+
+float get_pan_position(float begin_pan, float end_pan, double phi_prime) {
+    float true_end = end_pan - begin_pan;
+    float nom_position = true_end * (float)(phi_prime / EIGEN_PI) + (float)0.5 * true_end;
+    if (true_end < 0) {
+        float first_pass = std::min<float>(0, nom_position);
+        return std::max<float>(true_end, first_pass);
+    }
+    float first_pass = std::max<float>(0, nom_position);
+    return std::min<float>(true_end, first_pass);
+}
+
+float get_tilt_position(float begin_tilt, float end_tilt, double theta_prime) {
+    float true_end = end_tilt - begin_tilt;
+    float nom_position = 3 * true_end * (float)(theta_prime / EIGEN_PI) / 2;
+    if (true_end < 0) {
+        float first_pass = std::min<float>(0, nom_position);
+        return std::max<float>(true_end, first_pass);
+    }
+    float first_pass = std::max<float>(0, nom_position);
+    return std::min<float>(true_end, first_pass);
 }
